f815560485d84e33dfb0c5d77c7e9cfe4d0ae44e
Pedro Fernandes <pmcfernandes@gmail.com>
2022-11-27 01:40:59 +0000

    Added projects and find parameter

--- readme.md
+++ readme.md
@@ -2,9 +2,29 @@
 Create tasks with console and python
 
 #### Usage
-    task.py list
-    task.py add <task> [--due=<date>]
-    task.py <id> change [<task>] [--due=<date>] [--mark=<mark>]
+    task.py list [--project=<project>]
+    task.py find <regex> [--project=<project>]
+    task.py add <task> [--due=<date>] [--project=<project>]
+    task.py <id> change [<task>] [--due=<date>] [--mark=<mark>] [--project=<project>]
     task.py <id> resolved
     task.py <id> delete
+    task.py show projects
     task.py -h | --help
+
+#### examples
+    
+    python3 task.py list
+    python3 task.py list --project=supermarket
+    python3 task.py find "hello world \w*"
+    python3 task.py find "buy chocolate \w*" --project=supermarket
+    python3 task.py add "My first task"
+    python3 task.py add "My first task" --due=2weeks
+    python3 task.py add "buy chocolate for easter" --due=2weeks --project=supermarket
+    python3 task.py 1 change "hello task"
+    python3 task.py 1 change --due=1month
+    python3 task.py 1 change --project=easter
+    python3 task.py 1 resolved
+    python3 task.py 1 delete
+    python3 task.py show projects
+
+    
--- task.py
+++ task.py
@@ -1,11 +1,13 @@
 """Task console app. Add and list your life tasks
 
 Usage:
-  task.py list
-  task.py add <task> [--due=<date>]
-  task.py <id> change [<task>] [--due=<date>] [--mark=<mark>]
+  task.py list [--project=<project>]
+  task.py find <regex> [--project=<project>]
+  task.py add <task> [--due=<date>] [--project=<project>]
+  task.py <id> change [<task>] [--due=<date>] [--mark=<mark>] [--project=<project>]
   task.py <id> resolved
   task.py <id> delete
+  task.py show projects
   task.py -h | --help
 
 Options:
@@ -21,6 +23,31 @@ from dateutil.relativedelta import relativedelta
 import sqlite3
 import os
 import re
+
+
+def getProjectByName(conn, name: str) -> int:
+    cur = conn.cursor()
+    cur.execute(f"SELECT id FROM projects WHERE name LIKE '{name}'")
+    rows = cur.fetchall()
+
+    if len(rows) == 1:
+        return int(rows[0][0])
+    elif len(rows) == 0:
+        cur.execute("""
+            INSERT INTO projects (name) VALUES (?);
+        """, (name, ))
+
+        inserted_id = cur.lastrowid
+
+        try:
+            conn.commit()
+        except:
+            conn.rollback()
+            raise Exception(f"Can't create a new project with name '{name}'.")
+
+        return inserted_id
+    else:
+        return 0
 
 
 def calculateDueDate(due: str):
@@ -56,7 +83,11 @@ if __name__ == "__main__":
     if firstTime:
         cur = conn.cursor()
         cur.execute("""
-            CREATE TABLE tasks (id INTEGER PRIMARY KEY AUTOINCREMENT, title text, dt int, due int, resolved int);
+            CREATE TABLE tasks (id INTEGER PRIMARY KEY AUTOINCREMENT, project_id int, title text, dt int, due int, resolved int);
+        """)
+
+        cur.execute("""
+            CREATE TABLE projects (id INTEGER PRIMARY KEY AUTOINCREMENT, name text);
         """)
 
     if arguments["add"]:
@@ -64,9 +95,9 @@ if __name__ == "__main__":
             text = str(arguments["<task>"])
 
             cur = conn.cursor()
-            cur.execute(f"""
-                INSERT INTO tasks (title, dt, resolved) VALUES (?, ?, ?);
-            """, (text, time(), 0))
+            cur.execute("""
+                INSERT INTO tasks (project_id, title, dt, resolved) VALUES (?, ?, ?, ?);
+            """, (0, text, time(), 0))
 
             task_id = cur.lastrowid
 
@@ -77,6 +108,15 @@ if __name__ == "__main__":
                 cur.execute(f"""
                     UPDATE tasks SET due = ? WHERE id = {task_id} 
                 """, (due_date,))
+
+            if arguments["--project"] is not None:
+                project = str(arguments["--project"])
+                project_id = getProjectByName(conn, project)
+
+                if project_id > 0:
+                    cur.execute(f"""
+                        UPDATE tasks SET project_id = ? WHERE id = {task_id} 
+                    """, (project_id,))
 
             try:
                 conn.commit()
@@ -110,6 +150,15 @@ if __name__ == "__main__":
             cur.execute(f"""
                 UPDATE tasks SET resolved = ? WHERE id = {task_id} 
             """, (resolved,))
+
+        if arguments["--project"] is not None:
+            project = str(arguments["--project"])
+            project_id = getProjectByName(conn, project)
+
+            if project_id > 0:
+                cur.execute(f"""
+                   UPDATE tasks SET project_id = ? WHERE id = {task_id} 
+               """, (project_id,))
 
         try:
             conn.commit()
@@ -150,19 +199,37 @@ if __name__ == "__main__":
                 except:
                     conn.rollback()
 
-    if arguments['list']:
+    if arguments['list'] or arguments['find']:
         i = 0
 
         table = Table(show_header=True, header_style="bold magenta")
-        table.add_column("Id", style="dim", width=12)
-        table.add_column("Date", style="dim", width=12)
-        table.add_column("Due", style="dim", width=12)
+        table.add_column("Id", style="dim", width=6)
+        table.add_column("Date", style="dim", width=10)
+        table.add_column("Due", style="dim", width=10)
         table.add_column("Title")
+
+        if arguments["--project"] is None:
+            table.add_column("Project")
 
         # Get rows
         cur = conn.cursor()
-        rows = cur.execute("SELECT id, dt, title, due FROM tasks WHERE resolved = 0")
+        rows = cur.execute("""
+            SELECT tasks.id, dt, title, due, projects.id AS project_id, projects.name as project_name 
+            FROM tasks
+                LEFT JOIN projects ON projects.id = tasks.project_id 
+            WHERE resolved = 0
+        """)
         for row in rows:
+            if arguments['find']:
+                regex = str(arguments["<regex>"])
+                if re.match(rf'{regex}', str(row[2])) is None:
+                    continue
+                else:
+                    if arguments["--project"] is not None:
+                        project = str(arguments["--project"])
+                        if str(row[5]) != project:
+                            continue
+
             i += 1
             inserted_date = datetime.utcfromtimestamp(int(row[1])).strftime('%Y-%m-%d')
 
@@ -171,9 +238,25 @@ if __name__ == "__main__":
             else:
                 due_date = datetime.utcfromtimestamp(int(row[3])).strftime('%Y-%m-%d')
 
-            table.add_row(str(row[0]), inserted_date, due_date, str(row[2]))
+            if arguments["--project"] is None:
+                table.add_row(str(row[0]), inserted_date, due_date, str(row[2]), str(row[5]))
+            else:
+                table.add_row(str(row[0]), inserted_date, due_date, str(row[2]))
 
         console.print(table)
-        console.print("Total tasks: {i}".format(i=i))
+        console.print("Total tasks: {total}".format(total=i))
+
+    if arguments["show"]:
+        if arguments["projects"]:
+            table = Table(show_header=True, header_style="bold magenta")
+            table.add_column("Name")
+
+            # Get rows
+            cur = conn.cursor()
+            rows = cur.execute("SELECT name from projects")
+            for row in rows:
+                table.add_row(str(row[0]))
+
+            console.print(table)
 
     conn.close()
